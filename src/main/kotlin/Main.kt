60218420fc42ac13647704b885def061c25fc7d4
Joni De Borger <56124097+jonideborger@users.noreply.github.com>
2022-05-02 09:06:05 +0200

    changed comments

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,9 @@
 fun main(args: Array<String>) {
     val reader = FileReader();
 
-    //println(reader.getPuzzle1())
+    println(reader.getPuzzle1())
     //println(reader.getPuzzle2())
     //println(reader.getPuzzle3())
     //println(reader.getPuzzle4Numbers())
-    println(reader.getPuzzle4Cards())
+    //println(reader.getPuzzle4Cards())
 }
